01b15ff370523fd4544246b93250ba4c73c14f4a
manu3618 <manu+github@hbrt.eu>
2021-12-28 18:34:50 +0100

    Wire crate place

--- miniature_adventure/src/main.rs
+++ miniature_adventure/src/main.rs
@@ -1,3 +1,7 @@
+mod place;
+
 fn main() {
+    let room = place::build_empty_place();
     println!("Hello, world!");
+    println!("{}", room.description)
 }
--- miniature_adventure/src/place/mod.rs
+++ miniature_adventure/src/place/mod.rs
@@ -1,19 +1,26 @@
 enum PlaceType {
     //kind of place
-    room,
-    outdoor,
+    Room,
+    Outdoor,
 }
 
-struct Place {
+struct Objects {
     description: String,
+    transportable: bool,
+}
+
+pub struct Place {
+    pub description: String,
     explored: u64,         // number of time this place has been explored/searched
     objects: Vec<Objects>, // objectes that can be found here
     place_type: PlaceType,
 }
 
-fn build_empty_place() -> Place {
+pub fn build_empty_place() -> Place {
     Place {
         description: String::from("This place is empty"),
         explored: 0,
+        objects: Vec::new(),
+        place_type: PlaceType::Room,
     }
 }
