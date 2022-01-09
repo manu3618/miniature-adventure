eb9250588933203ffcd796fd71376927518e70ec
manu3618 <manu+github@hbrt.eu>
2022-01-09 15:12:00 +0100

    WIP on place graphs

--- miniature_adventure/src/main.rs
+++ miniature_adventure/src/main.rs
@@ -3,5 +3,6 @@ mod place;
 fn main() {
     let room = place::Place::new();
     println!("Hello, world!");
-    println!("{}", room.describe())
+    println!("{}", room.describe());
+    println!("{}", room.describe());
 }
--- miniature_adventure/src/place.rs
+++ miniature_adventure/src/place.rs
@@ -1,37 +1,47 @@
-enum PlaceType {
-    //kind of place
-    Room,
-    Outdoor,
-}
+use uuid::Uuid;
 
-struct Objects {
-    description: String,
-    transportable: bool,
+enum PlaceType {
+    /// kind of places
+    Inside,
+    Outside,
 }
 
 pub struct Place {
+    /// Place representation
+    ///
+    /// During the adventure, the user travel throught different places.
+    pub id: Uuid,
     pub description: String,
-    explored: u64,         // number of time this place has been explored/searched
-    objects: Vec<Objects>, // objectes that can be found here
+    explored: u64, // number of time this place has been explored/searched
     place_type: PlaceType,
+    exits: Vec<Place>, // link to other places
 }
 
 impl Place {
     pub fn new() -> Place {
         Place {
-            description: String::from("This place is empty"),
+            id: Uuid::new_v4(),
+            description: String::from("This place is empty."),
             explored: 0,
-            objects: Vec::new(),
-            place_type: PlaceType::Room,
+            place_type: PlaceType::Inside,
+            exits: Vec::new(),
         }
     }
     pub fn describe(&self) -> String {
-        let descr = self.description.clone();
-        //TODO enhance description with other atttributes
-        descr
+        let mut descr: Vec<&str> = Vec::new();
+        match &self.place_type {
+            PlaceType::Inside => descr.push("You are in a room."),
+            PlaceType::Outside => descr.push("You are outdoor."),
+        }
+        descr.push(&self.description);
+
+        let mut full_description = String::new();
+        for sentence in descr {
+            full_description = full_description + " " + sentence;
+        }
+        return full_description.trim().to_string();
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -44,6 +54,16 @@ mod tests {
             description: String::from(""),
             ..place
         };
-        assert_eq!(place.describe(), "");
+        assert_eq!(place.describe(), "You are in a room.");
+    }
+    fn place_description_determinism() {
+        let place = Place::new();
+        assert_eq!(place.describe(), place.describe());
+    }
+    fn self_reference_place() {
+        let place = Place::new();
+        let place_id = place.id;
+
+        // place.exits = [place].to_vec(); // XXX
     }
 }
