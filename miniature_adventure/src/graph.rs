9155622e764b73a6bcb9dba02e255143bbbce9e2
manu3618 <manu+github@hbrt.eu>
2022-03-27 16:33:53 +0200

    [GRAPH] Add children addition

--- miniature_adventure/src/graph.rs
+++ miniature_adventure/src/graph.rs
@@ -15,7 +15,32 @@ impl<T> Node<T> {
         Self::None
     }
     /// add new node with element
-    pub fn add_child(&mut self, new_item: T) {}
+    pub fn add_child(&mut self, new_item: T) {
+        let mut child = Self::Node {
+            item: Some(new_item),
+            id: "".to_string(),
+            parents: Box::new(Vec::new()),
+            children: Box::new(Vec::new()),
+        };
+
+        match self {
+            Self::None => {}
+            Self::Node { id, children, .. } => {
+                let parent_id = id.clone();
+                match child {
+                    Self::Node { ref mut id, .. } => {
+                        let child_id =
+                            format!("{}_{}_{}", parent_id, children.capacity(), children.len());
+                        *id = String::from(child_id);
+                    }
+                    _ => {
+                        panic!("Child not initialized as expected")
+                    }
+                }
+                children.push(child)
+            }
+        }
+    }
     /// link to new parent
     pub fn add_parent(self, parent: Node<T>) {}
     /// return vector of parents
